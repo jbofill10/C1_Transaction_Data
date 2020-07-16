ec2f613d2193e3e5a650b428d151c4387895ee8d
jbofill10 <juan.bofill10@gmail.com>
2020-07-16 01:32:27 -0400

    Add minor improvements

--- Main.py
+++ Main.py
@@ -1,39 +1,12 @@
-from tqdm import tqdm
-
 import pandas as pd
-import os, json
-
 import numpy as np
 
 
 def main():
 
-    dataset_content = list()
-    cols = None
-
-    # Way faster than use pd.concat every line
-    # Tricky needing me to sort the dict ;)
-
-    with open('Data/transactions.txt', 'r') as file:
-        lines = file.readlines()
-
-        for line in tqdm(lines):
-            temp_dict = json.loads(line)
-            temp_dict = dict(sorted(temp_dict.items()))
-
-            if cols is None: cols = list(temp_dict.keys())
-            values = list(temp_dict.values())
-            for i in range(len(values)):
-
-                if isinstance(values[i], str) and len(values[i]) == 0:
-                    values[i] = np.nan
-                elif isinstance(values[i], str):
-                    values[i] = values[i].strip()
-
-            dataset_content.append(values)
-
-    df = pd.DataFrame(dataset_content, columns=cols)
-
+    df = pd.read_json('Data/transactions.txt', lines=True)
+    df.replace('', np.nan, inplace=True)
+    print(df.isnull().sum())
     pd.to_pickle(df, 'Data/pickles/dataset', protocol=4)
 
 
